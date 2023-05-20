50fa266168d78e4c20017d2d869e78d5e4c3bfbc
ricky-kiva <ricky.kiva@gmail.com>
2023-05-20 09:39:00 +0700

    add ScrollToTop component

--- app/src/main/java/com/rickyslash/lazylistapp/HeroList.kt
+++ app/src/main/java/com/rickyslash/lazylistapp/HeroList.kt
@@ -1,15 +1,24 @@
 package com.rickyslash.lazylistapp
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,17 +26,35 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.rickyslash.lazylistapp.model.HeroesData
 import com.rickyslash.lazylistapp.ui.theme.LazyListAppTheme
-
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
 
 @Composable
 fun HeroList(modifier: Modifier = Modifier) {
     Box(modifier = modifier) {
-        LazyColumn {
+        val scope = rememberCoroutineScope() // to run scope manually on button click
+        val listState = rememberLazyListState() // to remember LazyList's state
+        val showButton: Boolean by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 } // change state according given condition
+        }
+        LazyColumn(state = listState) {
             items(HeroesData.heroes, key = { it.id }) { hero ->
                 HeroListItem(name = hero.name, photoUrl = hero.photoUrl, modifier = Modifier
                     .fillMaxWidth()
                 )
             }
+        }
+        AnimatedVisibility(
+            visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            ScrollToTopButton(onClick = {
+                scope.launch { listState.scrollToItem(index = 0) }
+            })
         }
     }
 }
@@ -61,9 +88,26 @@ fun HeroListItem(name: String, photoUrl: String, modifier: Modifier = Modifier) 
 }
 
 @Composable
+fun ScrollToTopButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .shadow(elevation = 10.dp, shape = CircleShape)
+            .clip(shape = CircleShape)
+            .size(56.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.White,
+            contentColor = MaterialTheme.colors.primary
+        )
+    ) {
+        Icon(imageVector = Icons.Filled.KeyboardArrowUp, contentDescription = null)
+    }
+}
+
+@Composable
 @Preview(showBackground = true)
 fun LazyListAppPreview() {
-    LazyListAppTheme() {
+    LazyListAppTheme {
         HeroListItem("Rickyslash", "")
     }
 }
